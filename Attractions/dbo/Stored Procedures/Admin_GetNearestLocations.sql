63b8eff21fc66189a9f6d818988006dd6c8f703f
Srinivas.Polani <Srinivas.Polani@pharmerica.com>
2016-12-07 15:35:28 -0500

    schedulers

---
+++ Attractions/dbo/Stored Procedures/Admin_GetNearestLocations.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE Admin_GetNearestLocations
+	
+AS
+BEGIN
+	
+SELECT [AttractionsId]
+      ,[AttractionName]
+      ,[AddressOne]
+      ,[AddressTwo]
+      ,[CityId]
+      ,[CategoryId]
+      ,[Longitude]
+      ,[Latitude]
+      ,[PlaceId]
+      ,[RankId]
+      ,[CreatedDate]
+      ,[CreatedBy]
+      ,[ModifiedDate]
+      ,[ModifiedBy]
+      ,[GoogleSearchText]
+      ,[GoogleWebSite]
+      ,[GoogleICon]
+      ,[GoogleInternational_phone_number]
+      ,[Googleadr_address]
+      ,[GoogleName]
+      ,[GoogleRating]
+      ,[GoogleUser_ratings_total]
+  FROM [dbo].[Attractions]
+  WHERE PlaceId IS NULL
+  OR PlaceId = ''
+  OR Longitude IS NULL
+  OR Longitude = ''
+  OR Latitude IS NULL
+  OR Latitude = ''
+
+
+END
+GO
